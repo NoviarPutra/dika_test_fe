353ee0cb954d70af64f505f2435d91ec723b8fa0
Noviar Putra Budiawan <45009698+NoviarPutra@users.noreply.github.com>
2023-07-26 05:21:49 +0700

    first init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Landing, Missing } from "./pages";
+import { Navigation } from "./components/atoms";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route index element={<Landing />} />
+        <Route path='*' element={<Missing />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import Landing from "./Landing/Landing";
+import Missing from "./Missing/Missing";
+
+export { Landing, Missing };
